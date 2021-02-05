ee16fb5982a4fdffbd59a315285bfe153e90b148
wanabe <s.wanabe@gmail.com>
2021-02-05 14:59:08 +0900

    fix: fix: allow to call InMemoryCache methods in parallel with mutex

--- in_memory_cache.go
+++ in_memory_cache.go
@@ -1,11 +1,16 @@
 package avroturf
 
-import "github.com/hamba/avro"
+import (
+	"sync"
+
+	"github.com/hamba/avro"
+)
 
 type InMemoryCache struct {
 	SchemasByID             map[uint32]avro.Schema
 	IdsBySchema             map[string]uint32
 	SchemasBySubjectVersion map[string]avro.Schema
+	sync.Mutex
 }
 
 func NewInMemoryCache() *InMemoryCache {
@@ -17,21 +22,29 @@ func NewInMemoryCache() *InMemoryCache {
 }
 
 func (c *InMemoryCache) LookupSchemaByID(schemaID uint32) avro.Schema {
+	c.Lock()
+	defer c.Unlock()
 	return c.SchemasByID[schemaID]
 }
 
 func (c *InMemoryCache) StoreSchemaByID(schemaID uint32, schema avro.Schema) avro.Schema {
+	c.Lock()
+	defer c.Unlock()
 	c.SchemasByID[schemaID] = schema
 	return schema
 }
 
 func (c *InMemoryCache) LookupIdBySchema(subject string, schema avro.Schema) uint32 {
 	key := subject + schema.String()
+	c.Lock()
+	defer c.Unlock()
 	return c.IdsBySchema[key]
 }
 
 func (c *InMemoryCache) StoreIdBySchema(subject string, schema avro.Schema, schemaID uint32) uint32 {
 	key := subject + schema.String()
+	c.Lock()
+	defer c.Unlock()
 	c.IdsBySchema[key] = schemaID
 	return schemaID
 }
